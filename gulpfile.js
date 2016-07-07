e099327b3595dc2775372315ee396eb22876e94a
Jenn Simbartl <jsimbartl_2017@depauw.edu>
2016-07-07 09:16:57 -0400

    Add gulp.

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 (function() {
   'use strict';
   var gulp = require('gulp');
+  var order = require('gulp-order');
+  var print = require('gulp-print');
   var concat = require('gulp-concat');
   var plumber = require('gulp-plumber');
   var sourcemaps = require('gulp-sourcemaps');
@@ -10,24 +12,30 @@
   gulp.task('bundle', bundle);
   gulp.task('start-webserver', startWebServer);
   gulp.task('watch', watch);
-  gulp.task('default', ['bundle', 'start-webserver', 'watch'])
+  gulp.task('default', ['bundle', 'start-webserver', 'watch']);
 
-  //////////////////////////////
+  //////////////////
 
   var jsFiles = [
     'app/**/*.js',
     '!app/bower_components/**/*',
-    '!app/content/bundle.js',
+    '!app/content/bundle.js'
   ];
 
   function bundle() {
     return gulp.src(jsFiles)
+    .pipe(order([
+      'app/app.module.js',              // put the main module first
+      'app/**/*.module.js',             // followed by all other modules
+      'app/**/*.js'                     // and all other JS files
+    ], { base: './' }))
       .pipe(plumber())                // restart gulp on error
-      .pipe(sourcemaps.init())        //let sourcemap watch this pipeline
-      .pipe(babel())                  //transpile into ES5 the browser understands
-      .pipe(concat('bundle.js'))      //concatenate all JS files
-      .pipe(sourcemaps.write('.'))    //emits sourcemap bundle.js.map for debugging
-      .pipe(gulp.dest('app/content')) //save bundle.js and bundle.js.map
+      .pipe(sourcemaps.init())        // let sourcemap watch this pipeline
+      .pipe(babel())                  // transpile into ES5 the browser understands
+      .pipe(print())
+      .pipe(concat('bundle.js'))      // concatenate all JS files
+      .pipe(sourcemaps.write('.'))    // emits sourcemap bundle.js.map for debugging
+      .pipe(gulp.dest('app/content')) // save bundle.js and bundle.js.map
   }
 
   function startWebServer() {
@@ -38,5 +46,4 @@
     gulp.watch('app/**/*', ['bundle']);
   }
 
-
-  })();
+})();
