6f333617a7f5b575b0c700db3e4253b4f1270dcb
Jenn Simbartl <jsimbartl_2017@depauw.edu>
2016-07-15 09:24:07 -0400

    Concatenate CSS files.

--- gulpfile.js
+++ gulpfile.js
@@ -8,33 +8,42 @@
   var babel = require('gulp-babel');
   var connect = require('gulp-connect');
   var uglify = require('gulp-uglify');
+  var cleanCSS = require('gulp-clean-css');
 
   gulp.task('bundle', bundle);
   gulp.task('vendor', vendor);
+  gulp.task('css', css);
   gulp.task('start-web-server', startWebServer);
   gulp.task('watch', watch);
-  gulp.task('default', ['bundle', 'vendor', 'start-web-server', 'watch']);
+  gulp.task('default', ['bundle', 'vendor', 'css', 'start-web-server', 'watch']);
 
   ////////////////////
 
   var jsFiles = [
     'app/**/*.js',
-    '!app/bower_components/**/*',
     '!app/content/bundle.js',
     '!app/content/vendor.js'
   ];
 
   var vendorFiles = [
-    'app/bower_components/jquery/dist/jquery.min.js',
-    'app/bower_components/bootstrap/dist/js/bootstrap.min.js',
-    'app/bower_components/angular-loader/angular-loader.min.js',
-    'app/bower_components/angular/angular.js',
-    'app/bower_components/angular-ui-router/release/angular-ui-router.min.js',
-    'app/bower_components/angular-flash-alert/dist/angular-flash.js',
-    'app/bower_components/textAngular/dist/textAngular-rangy.min.js',
-    'app/bower_components/textAngular/dist/textAngular-sanitize.min.js',
-    'app/bower_components/textAngular/dist/textAngular.min.js',
-    'app/bower_components/angular-http-loader/app/package/js/angular-http-loader.min.js'
+    'bower_components/jquery/dist/jquery.min.js',
+    'bower_components/bootstrap/dist/js/bootstrap.min.js',
+    'bower_components/angular-loader/angular-loader.min.js',
+    'bower_components/angular/angular.js',
+    'bower_components/angular-ui-router/release/angular-ui-router.min.js',
+    'bower_components/angular-flash-alert/dist/angular-flash.js',
+    'bower_components/textAngular/dist/textAngular-rangy.min.js',
+    'bower_components/textAngular/dist/textAngular-sanitize.min.js',
+    'bower_components/textAngular/dist/textAngular.min.js',
+    'bower_components/angular-http-loader/app/package/js/angular-http-loader.min.js',
+    'bower_components/angular-resource/angular-resource.min.js'
+  ];
+
+  var cssFiles = [
+    'bower_components/font-awesome/css/font-awesome.css',
+    'bower_components/bootstrap/dist/css/bootstrap.css',
+    'bower_components/textAngular/dist/textAngular.css',
+    'app/content/app.css'
   ];
 
   function bundle() {
@@ -66,6 +75,17 @@
       .pipe(gulp.dest('app/content'));
   }
 
+  function css() {
+    return gulp.src(cssFiles)
+      .pipe(order(cssFiles, { base: './' }))
+      .pipe(plumber())
+      .pipe(sourcemaps.init())
+      .pipe(cleanCSS())
+      .pipe(concat('bundle.css'))
+      .pipe(sourcemaps.write('.'))
+      .pipe(gulp.dest('app/content'));
+  }
+
   function startWebServer() {
     connect.server({
       root: 'app',
@@ -76,8 +96,10 @@
   function watch() {
     gulp.watch([
       'app/**/*',
-      'gulpfile.js',
       '!app/content/bundle.*',
-      '!app/content/vendor.*'], ['bundle', 'vendor']);
+      '!app/content/vendor.*'
+    ], ['bundle', 'css']);
+
+    gulp.watch(['bower_components/**/*'], ['vendor', 'css']);
   }
 })();
